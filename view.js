0f33f1b9dd23086bd5e071700d03047135339ca1
Joe Day <thejode@gmail.com>
2015-03-14 20:44:18 -0700

    we have dice

--- view.js
+++ view.js
@@ -4,19 +4,79 @@ function getNum(highNum) {
     highNum = 6;
   }
   var num = Math.floor((Math.random() * highNum) +1)
-  var el = document.getElementById("number")
-  var r = getRGB();
-  var g = getRGB();
-  var b = getRGB();
-  var rgb = "rgb("+parseInt(r)+","+parseInt(g)+","+parseInt(b)+")";
-  el.innerHTML = parseInt(num);
-  el.style.color = rgb;
+  
+  var el1 = document.getElementById("one");
+  var el2 = document.getElementById("two");
+  var el3 = document.getElementById("three");
+  var el4 = document.getElementById("four");
+  var el5 = document.getElementById("five");
+  var el6 = document.getElementById("six");
+  var el7 = document.getElementById("seven");
+
+  
+  // el.innerHTML = parseInt(num);
+  // el.style.color = rgb;
+  
+  if (num === 6) {
+    el1.style.background = getRGB();
+    el2.style.background = getRGB();
+    el3.style.background = getRGB();
+    el4.style.background = "rgb(255,255,255)";
+    el5.style.background = getRGB();
+    el6.style.background = getRGB();
+    el7.style.background = getRGB();
+  } else if (num == 5) {
+    el1.style.background = getRGB();
+    el2.style.background = "rgb(255,255,255)";
+    el3.style.background = getRGB();
+    el4.style.background = getRGB();
+    el5.style.background = getRGB();
+    el6.style.background = "rgb(255,255,255)";
+    el7.style.background = getRGB();
+  } else if (num == 4) {
+    el1.style.background = getRGB();
+    el2.style.background = "rgb(255,255,255)";
+    el3.style.background = getRGB();
+    el4.style.background = "rgb(255,255,255)";
+    el5.style.background = getRGB();
+    el6.style.background = "rgb(255,255,255)";
+    el7.style.background = getRGB();
+  } else if (num == 3) {
+    el1.style.background = getRGB();
+    el2.style.background = "rgb(255,255,255)";
+    el3.style.background = "rgb(255,255,255)";
+    el4.style.background = getRGB();
+    el5.style.background = "rgb(255,255,255)";
+    el6.style.background = "rgb(255,255,255)";
+    el7.style.background = getRGB();
+  } else if (num == 2) {
+    el1.style.background = getRGB();
+    el2.style.background = "rgb(255,255,255)";
+    el3.style.background = "rgb(255,255,255)";
+    el4.style.background = "rgb(255,255,255)";
+    el5.style.background = "rgb(255,255,255)";
+    el6.style.background = "rgb(255,255,255)";
+    el7.style.background = getRGB();
+  } else if (num == 1) {
+    el1.style.background = "rgb(255,255,255)";
+    el2.style.background = "rgb(255,255,255)";
+    el3.style.background = "rgb(255,255,255)";
+    el4.style.background = getRGB();
+    el5.style.background = "rgb(255,255,255)";
+    el6.style.background = "rgb(255,255,255)";
+    el7.style.background = "rgb(255,255,255)";
+  }
+  
 }
 
 function getRGB() {
-  var num = Math.floor((Math.random() * 255) +1)
-  console.log(num)
-  return num;
+  
+  var r = Math.floor((Math.random() * 255) +1)
+  var g = Math.floor((Math.random() * 255) +1)
+  var b = Math.floor((Math.random() * 255) +1)
+  var rgb = "rgb("+parseInt(r)+","+parseInt(g)+","+parseInt(b)+")";
+  
+  return rgb;
 }
 
 
